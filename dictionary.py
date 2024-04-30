a65a5d44e1facea32d69c6cd76e1541281ca19c5
taratirtanata <150347710+taratirtanata@users.noreply.github.com>
2024-04-30 17:55:18 +0700

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+# a = dict()
+# a["tara"] = ["sushi", "salmon", "mentai", "carbonara"]
+# a["mingfee"] = ["ayam goreng", "hati", "kentang", "ice cream"]
+# a["ukuran sepatu"] = [39]
+# print(a)
+
+#output:
+#{'tara': ['sushi', 'salmon', 'mentai', 'carbonara'], 'mingfee': ['ayam goreng', 'hati', 'kentang', 'ice cream'], 'ukuran sepatu': [39]}
+
+#_______________________________________________________________________________________________
+
+# a = dict()
+# for i in range(2):
+#     var = dict()
+#     var["nama"] = input("Silahkan masukkan nama: ")
+#     var["nohp"] = int(input("Silahkan masukkan no. hp: "))
+#     nim = int(input("Silahkan masukkan NIM: "))
+#     a[nim] = var
+# print(a)
+
+#output:
+# {71231056: {'nama': 'Tara', 'nohp': 89654489839}, 71220960: {'nama': 'Ming Fee', 'nohp': 911}}
+
+#_______________________________________________________________________________________________
